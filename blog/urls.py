a5a4d24ec7317446c7a2aae9237ac49bf0e2b55f
Darin <drnellman@gmail.com>
2020-02-05 16:40:20 +0200

    changes to urls & views.py - from about a week ago I think

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('about/',views.AboutView.as_view(),name='about'),
     path('post/<int:blog_id>/', views.PostDetailView.as_view, name='post_detail'),
     path('post/new/', views.CreatePOstView.as_view(),name='post_new'),
-    path('post/<int:blog_id>/edit', views.PostUpdateView.as_view(), name='post_edit'),
-    path('post/<int:blog_id>/remove', views.PostDeleteView.as_view(), name='post_remove'),
+    path('post/<int:post_id>/edit', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:post_id>/remove', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
-    
+    path('post/<int:post_id>/comment/',views.add_comment_to_post,name='add_comment_to_post'),
+
 ]
